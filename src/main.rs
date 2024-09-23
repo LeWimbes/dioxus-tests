ac2535c3aeb85d24ea01ffcc3bff6128250c5f99
LeWimbes <12753781+LeWimbes@users.noreply.github.com>
2024-09-23 17:23:27 +0200

    routing via signal

--- src/main.rs
+++ src/main.rs
@@ -27,21 +27,30 @@ fn App() -> Element {
 
 #[component]
 fn Page(url_fragment: ReadOnlySignal<String>) -> Element {
-    let mut fragment: Signal<String> = use_signal(|| url_fragment());
+    let fragment: Signal<String> = use_signal(|| url_fragment());
+    let mut new_fragment: Signal<String> = use_signal(|| " ".into());
 
+    // use_effect is needed in case the element in question isn't rendered yet
     use_effect(move || {
-        let document = window().unwrap().document().unwrap();
-        if let Some(element) = document.get_element_by_id(&fragment()) {
-            element.scroll_into_view_with_bool(true);
+        if !url_fragment().is_empty() {
+            info!("Scrolling to fragment: '{}'", url_fragment());
+            let document = window().unwrap().document().unwrap();
+            if let Some(element) = document.get_element_by_id(&url_fragment()) {
+                element.scroll_into_view();
+            }
         }
     });
 
-    let mut update_fragment = move |new_fragment: String| {
-        if new_fragment != url_fragment() {
-            navigator().push(Route::Page { url_fragment: new_fragment.clone() });
-            fragment.set(new_fragment);
-        }
-    };
+    if new_fragment() == url_fragment() {
+        info!("Navigation happened already!");
+    } else if new_fragment() == " " {
+        info!("Went back (or loaded page).");
+    } else {
+        info!("Navigating to new fragment: '{}'", new_fragment());
+        let url_fragment = new_fragment().clone();
+        new_fragment.set(" ".into());
+        navigator().push(Route::Page { url_fragment });
+    }
 
     rsx! {
         p {
@@ -50,10 +59,18 @@ fn Page(url_fragment: ReadOnlySignal<String>) -> Element {
         p { "Current fragment: '{fragment()}'" }
 
         div { class: "h-96" }
-        h1 { id: "section1", onclick: move |_| update_fragment("section1".into()), "Section1" }
+        Sec { title: "Section1", new_fragment }
         div { class: "h-96" }
-        h1 { id: "section2", onclick: move |_| update_fragment("section2".into()), "Section2" }
+        Sec { title: "Section2", new_fragment }
         div { class: "h-[300rem]" }
+    }
+}
+
+#[component]
+fn Sec(title: String, new_fragment: Signal<String>) -> Element {
+    let id = title.to_lowercase().replace(' ', "-");
+    rsx! {
+        h1 { id: "{id}", onclick: move |_| new_fragment.set(id.clone()), "{title}" }
     }
 }
 

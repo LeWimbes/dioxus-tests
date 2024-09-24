3815de176d4dbac9113ed94b1320c713964d3ac4
LeWimbes <12753781+LeWimbes@users.noreply.github.com>
2024-09-24 19:59:50 +0200

    some improvements

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 use dioxus::prelude::*;
 use dioxus_logger::tracing::{info, Level};
 use std::rc::Rc;
-use web_sys::window;
+use web_sys::{window, ScrollIntoViewOptions, ScrollToOptions};
 
 #[derive(Clone, Routable, Debug, PartialEq)]
 enum Route {
@@ -42,19 +42,42 @@ impl NavigationHandler {
 
 #[component]
 fn Page(url_fragment: ReadOnlySignal<String>) -> Element {
+    let scroll_to = |id: String| {
+        if !id.is_empty() {
+            let window = window().unwrap();
+            let document = window.document().unwrap();
+
+            if let Some(element) = document.get_element_by_id(&id) {
+                let offset = if let Some(navbar) = document.get_element_by_id("navbar") {
+                    let navbar_height = navbar.get_bounding_client_rect().height();
+                    (navbar_height / 5.0) * 6.0
+                } else {
+                    96.0 // default value in case 1rem = 16px
+                };
+
+                let body_rect = document.body().unwrap().get_bounding_client_rect();
+                let element_rect = element.get_bounding_client_rect();
+
+                let element_position = element_rect.top() - body_rect.top();
+                let offset_position = element_position - offset;
+
+                let options = ScrollToOptions::new();
+                options.set_top(offset_position);
+
+                window.scroll_to_with_scroll_to_options(&options);
+            }
+        }
+    };
 
     // use_effect is needed in case the element in question isn't rendered yet
     use_effect(move || {
-        if !url_fragment().is_empty() {
-            let document = window().unwrap().document().unwrap();
-            if let Some(element) = document.get_element_by_id(&url_fragment()) {
-                element.scroll_into_view();
-            }
-        }
+        scroll_to(url_fragment());
     });
 
     let handler = NavigationHandler(Rc::new(move |new_fragment: String| {
-        if new_fragment != url_fragment() {
+        if new_fragment == url_fragment() {
+            scroll_to(new_fragment);
+        } else {
             navigator().replace(Route::Page { url_fragment: new_fragment });
         }
     }));
@@ -62,6 +85,8 @@ fn Page(url_fragment: ReadOnlySignal<String>) -> Element {
     use_context_provider(|| handler);
 
     rsx! {
+        Navbar {}
+
         p {
             Link { to: Route::Home {}, "Go Home" }
         }
@@ -84,9 +109,21 @@ fn Sec(title: String) -> Element {
     }
 }
 
+pub fn Navbar() -> Element {
+    rsx! {
+        nav { class: "fixed top-0 z-50 px-8 w-full flex flex-col items-center bg-black shadow-md h-20",
+            id: "navbar",
+        }
+        // take up space of navbar
+        div { class: "h-20" }
+    }
+}
+
 #[component]
 fn Home() -> Element {
     rsx! {
+        Navbar {}
+
         p {
             Link {
                 to: Route::Page {
@@ -97,6 +134,9 @@ fn Home() -> Element {
         }
         p {
             Link { to: Route::from_route_segment("/page").unwrap(), "Go to page from route segment" }
+        }
+        p {
+            Link { to: Route::from_route_segment("/page#section1").unwrap(), "Go to page section1 from route segment" }
         }
         p {
             Link {
